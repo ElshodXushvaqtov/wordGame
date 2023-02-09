8ff23666b9fea073b09a7df1459d2aa0997f6212
PC673 <elshodxushvaqtov07@gmail.com>
2023-02-09 13:20:31 +0500

    SozOyni

--- app/src/main/java/uz/itschool/myapplication/gameActivity.kt
+++ app/src/main/java/uz/itschool/myapplication/gameActivity.kt
@@ -3,11 +3,9 @@ package uz.itschool.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_game)
     }
 }
